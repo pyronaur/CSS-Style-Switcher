b118e56266a8935a24aa380ed274e14c9692c1fa
Fellipe Vasconcelos da Silva <fellipe.fvs@gmail.com>
2012-12-23 15:55:06 -0200

    Support for multiple CSS files at once
    Support for multiple CSS files at once

--- switcher.php
+++ switcher.php
@@ -1,81 +1,39 @@
 <?php
-function filter_css($file) {
 
-	$file = explode('.', $file);
-	$extension = $file[count($file) - 1];
-
-	if($extension == "css") {
-		return TRUE;
-	}
-	else {
-		return FALSE;
-	}
-
-}
-
-function default_css() {
-
-	if(file_exists('style.css')) {
-		$default = 'style.css';
-	}
-	elseif(file_exists('default.css')) {
-		$default = 'default.css';
-	}
-	else {
-
-		$files = scandir(".");
-		$file = array_filter($files, 'filter_css');
-		$file = explode('-', implode('-', $file));
-
-		$default = $file[0];
+function default_css(){
+	$default = check_file_exists(array('light.css'));
+	if(empty($default)){
+		$default = array_values(array_filter(scandir('.'), 'filter_css'));
+		return array($default[0]);
 	}
 	return $default;
-
 }
 
+function filter_css($file){
+	$file = explode('.', $file);
+	return $file[count($file) - 1] == 'css';
+}
 
+function check_file_exists($file){
+	foreach($file as $key => $value)
+		if(!file_exists($value))
+			unset($file[$key]);
+	return array_unique($file);
+}
 
+function get_css($entry){
+	$include = check_file_exists(explode('+', $entry));
+	return (!empty($include)) ? $include : default_css();
+}
 
-# Check for a cookie and set a cookie
+if(isset($_COOKIE['css_stylesheet']) && !isset($_GET['style'])){
+		$include = get_css($_COOKIE['css_stylesheet']);
+} else {
+		$include = (isset($_GET['style'])) ? get_css($_GET['style']) : default_css();
+}
+setcookie('css_stylesheet', implode('+', $include));
 
-            if(isset($_COOKIE['css_stylesheet']) && !isset($_GET['style'])) {
-
-		            $include = (file_exists($_COOKIE['css_stylesheet'])) ? $_COOKIE['css_stylesheet'] : default_css();
-
-	       
-             }
-             
-            else {
-		            # First of all, we check if there is any data passed, and if there isn't, we use the else statement:
-		            if(isset($_GET)) {
-
-				            if(!file_exists($_GET['style'])) {
-					            $include = default_css();
-				            }
-				            else {
-					            $include = $_GET['style'];
-				            }
-		            }
-
-	                        # If any data isn't passed with the $_GET variable, we set a default style
-	                        else {
-		                        $include = default_css();
-
-	                        }
-	                        setcookie('css_stylesheet', $include);
-
-	                       
-
-                    }   
-
-	            # Secure the include and Include it!
-		                 $file = explode('.', $include);
-	                     $extension = $file[count($file) - 1];
-                # Include, if the coast is clear
-                header("Content-type: text/css");
-                if(isset($_GET['style']))
-                (isset($_SERVER['HTTP_REFERER'])) ?	header("Location:".$_SERVER['HTTP_REFERER']) : header("Location:http://".$_SERVER['SERVER_NAME']);
-                ($extension == "css") ? include $include : die("Please insert a .css file");
-                
-
-?>
+header('Content-type: text/css');
+if(isset($_GET['style']))
+	(isset($_SERVER['HTTP_REFERER'])) ?	header("Location:".$_SERVER['HTTP_REFERER']) : header("Location:http://".$_SERVER['SERVER_NAME']);
+if(!empty($include)) { foreach($include as $value) include $value; } else { die(".css NOT FOUND"); }
